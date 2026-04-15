e71370e978aaffd0097f491c62295633015a5a59
jhpark <jhpark@ipacom.co.kr>
2026-04-15 15:31:15 +0900

    "feat: add Google Analytics"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { NavigationProgress } from "@/components/navigation-progress";
@@ -67,6 +68,18 @@ export default function RootLayout({
         className="min-h-full flex flex-col bg-background text-foreground"
         suppressHydrationWarning
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DKFS7ZXMN7"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DKFS7ZXMN7');
+          `}
+        </Script>
         <NavigationProgress />
         <ThemeProvider>{children}</ThemeProvider>
       </body>
